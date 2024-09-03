78d0fee15eab7d8403fece95c41ee764983fe6ac
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-09-03 15:10:04 +0300

    feat(profile): Set `xwayland:force_zero_scaling` based on workspace

--- src/profile.rs
+++ src/profile.rs
@@ -1,22 +1,24 @@
-use hyprland::{event_listener::EventListener, shared::WorkspaceType};
+use hyprland::{event_listener::EventListener, keyword::Keyword, shared::WorkspaceType};
 use std::{collections::HashMap, process::Command};
 
 pub struct Config {
     output: String,
     default_scale: f64,
-    workspace_map: HashMap<u8, f64>,
+    workspace_scale_map: HashMap<u8, f64>,
+    xwayland_scaling_workspaces: Vec<u8>,
 }
 
 impl Default for Config {
     fn default() -> Self {
-        let mut workspace_map = HashMap::new();
-        let _ = workspace_map.insert(7, 2.0);
-        let _ = workspace_map.insert(10, 1.0);
+        let mut map = HashMap::new();
+        let _ = map.insert(7, 2.0);
+        let _ = map.insert(10, 1.0);
 
         Self {
             output: String::from("eDP-1"),
-            workspace_map,
             default_scale: 1.5,
+            workspace_scale_map: map,
+            xwayland_scaling_workspaces: vec![7],
         }
     }
 }
@@ -26,7 +28,7 @@ pub fn setup_listener(event_listener: &mut EventListener, config: Config) {
         WorkspaceType::Special(_) => { /* Do nothing I guess? */ }
         WorkspaceType::Regular(id_string) => {
             let id = id_string.parse::<u8>().unwrap_or(1);
-            let scale = match config.workspace_map.get(&id) {
+            let scale = match config.workspace_scale_map.get(&id) {
                 Some(scale) => *scale,
                 None => config.default_scale,
             };
@@ -38,6 +40,12 @@ pub fn setup_listener(event_listener: &mut EventListener, config: Config) {
                 .arg(scale.to_string())
                 .output()
                 .inspect_err(|error| eprintln!("Couldn't set display scale: {error:?}"));
+
+            let scale_this_ws = config.xwayland_scaling_workspaces.contains(&id);
+            let _ = Keyword::set("xwayland:force_zero_scaling", (!scale_this_ws).to_string())
+                .inspect_err(|error| {
+                    eprintln!("Couldn't set `xwayland:force_zero_scaling` keyword: {error:?}");
+                });
         }
     });
 }
